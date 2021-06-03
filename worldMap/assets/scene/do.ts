59680835e162f6e62fab656e3c8843ea83e9b610
宇鑫 穆 <1363902011@qq.com>
2021-06-03 23:42:05 +0800

    游戏地图

---
+++ worldMap/assets/scene/do.ts
@@ -0,0 +1,34 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    click: cc.Label = null;
+
+   inload(){
+        if(this.click,node,on){
+            this.goToGame();
+        }
+   }
+   goToGame(){
+        cc.director.preloadScene('chiyanzhiyu',(err)=>{
+            if(!err){
+            cc.director.loadScene('chiyanzhiyu',function(){
+                });
+            }
+        });
+   }
+   update(dt){}
+   }
+
+
+    
+
